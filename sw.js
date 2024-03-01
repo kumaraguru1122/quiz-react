16b5d58ce69d659370e424308732c69336024729
kumaraguru1122 <124264978+kumaraguru1122@users.noreply.github.com>
2024-03-01 20:51:04 +0530

    added some pwa features

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = "version-1";
+const urlsToCache = ['index.html', 'offline.html'];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => Promise.all(
+            cacheNames.map((cacheName) => {
+                if (!cacheWhitelist.includes(cacheName)) {
+                    return caches.delete(cacheName);
+                }
+            }),
+        )),
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(() => fetch(event.request)
+                .catch(() => caches.match('offline.html')),
+            ),
+    );
+});
